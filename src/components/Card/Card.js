4c7d717eed71c3d2824bd6277cff2003cfddd670
Andrzej Sterniewski <drapiszon@gmail.com>
2023-11-09 20:10:48 +0100

    working on adding to favorite

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,15 @@
 import styles from './Card.module.scss';
+import { useState } from 'react';
 import Button from '../Button/Button';
 
 const Card = props => {
+
+    const [isFavorite, setFavorite] = useState(false);
+
     return (
-            <li className={styles.card}>{props.title}<Button><span className={styles.icon + ' fa fa-star-o'}></span></Button></li>
+            <li className={styles.card}>{props.title}
+            <Button onClick={setFavorite}><span className={styles.icon + ' fa fa-star-o'}></span></Button>
+            </li>
     );
 };
 
